fd7b86d4d26af32e4ad94d60d62100b28bb10f4d
SartisonYA <sartisonya@mer.ci.nsu.ru>
2024-10-31 12:14:12 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RunningButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            Canvas.SetLeft(runningButton, Canvas.GetLeft(runningButton) + random.Next(-200, 200));
+            Canvas.SetTop(runningButton, Canvas.GetTop(runningButton) + random.Next(-200, 200));
+
+        }
+
+        private void runningButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
